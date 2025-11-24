5e5ffa686563d1c67cbde359d6d72528f87a6de7
TwillightMoon <47419205+IPaleLuna@users.noreply.github.com>
2025-11-24 13:50:22 +0300

    Update HibernateUtil.java

--- src/main/java/com/userfish/util/HibernateUtil.java
+++ src/main/java/com/userfish/util/HibernateUtil.java
@@ -1,5 +1,33 @@
 package com.userfish.util;
 
-public class HibernateUtil {
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class HibernateUtil {
+    private static final Logger logger = LoggerFactory.getLogger(HibernateUtil.class);
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure("hibernate.cfg.xml");
+            return configuration.buildSessionFactory();
+        } catch (Exception e) {
+            logger.error("Initial SessionFactory creation failed", e);
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        if (sessionFactory != null) {
+            sessionFactory.close();
+            logger.info("Hibernate SessionFactory closed");
+        }
+    }
 }
